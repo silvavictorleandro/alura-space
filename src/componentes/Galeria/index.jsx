5edd391a497fece96c565b948abab0e8aa42870a
Victor Silva <silvavictorleandro@outlook.com>
2023-06-27 19:37:44 -0300

    implementando os cards na galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,14 +1,37 @@
 import Tags from "../Tags";
 import styles from "./Galeria.module.scss";
+import fotos from "./fotos.json";
+import favorito from "./favorito.png";
+import open from "./open.png";
 
 import React from 'react'
 
 export default function Galeria() {
   return (
-    <section>
+    <section className={styles.galeria}>
       <h2>Navegue pela galeria</h2>
       <Tags />
-      <ul></ul>
+      <ul className={styles.galeria__cards}>
+        {fotos.map((foto) => {
+          return (
+            <li key={foto.id} className={styles.galeria__card}>
+              <img 
+                className={styles.galeria__imagem}
+                src={foto.imagem}
+                alt={foto.titulo}
+              />
+              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <div>
+                <p>{foto.creditos}</p>
+                <span>
+                  <img src={favorito} alt="ícone coração de curtir" />
+                  <img src={open} alt="ícone de abrir modal" />
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   )
 }
