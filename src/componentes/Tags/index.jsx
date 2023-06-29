afebeea589bccb20f3dd864db4ee496889b84599
Victor Silva <silvavictorleandro@outlook.com>
2023-06-28 21:58:14 -0300

    adicionando funcionalidade de filtrar por tag

--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,16 +1,19 @@
 import styles from "./Tags.module.scss";
+import fotos from "../Galeria/fotos.json";
 
 import React from 'react'
 
-export default function Tags() {
+export default function Tags({ tags, filtraFotos, setItens }) {
   return (
     <div className={styles.tags}>
-      <p>Busque por tags:</p>
+      <p>Filtre por tags:</p>
       <ul className={styles.tags__lista}>
-        <li>Estrelas</li>
-        <li>Galáxias</li>
-        <li>Lua</li>
-        <li>Planetas</li>
+        {tags.map((tag) => {
+          return (
+            <li key={tag} onClick={() => filtraFotos(tag)}>{tag}</li>
+          )
+        })}
+        <li onClick={() => setItens(fotos)}>Todas</li>
       </ul>
     </div>
   )
